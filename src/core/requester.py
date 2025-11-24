9e6e19a185377cf8d12606d81da77a2cccc515f1
alilo113 <aliopdandan@gmail.com>
2025-11-24 11:46:39 +0100

    f

--- src/core/requester.py
+++ src/core/requester.py
@@ -1,10 +1,15 @@
 import requests
 
-def send_request(url):
-    r = requests.post(url, auth=('user', 'pass'))
-    
-    while r.status_code != 429:
-        print(f"Response Code: {r.status_code}")
-        r = requests.post(url, auth=('user', 'pass'))
-    
-    print("Received 429 Too Many Requests")
+def send_request(url, max_requests):
+    for request_attempt in range(max_requests):
+        try:
+            response = requests.post(url, auth=('user', 'pass'))
+            print(f"Request attempt {request_attempt + 1}: Status Code {response.status_code}")
+            if response.status_code == 429:
+                print("Received 429 Too Many Requests. Stopping further attempts.")
+                break
+
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(f"Request attempt {request_attempt + 1} failed: {e}")
+    return None
